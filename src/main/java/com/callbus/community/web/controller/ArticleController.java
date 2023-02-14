b60a29d4d72d36d83be7d2b6b7c1fbbc774aae50
wijoonwu <devjoon@nate.com>
2023-02-15 01:09:42 +0900

    Refactor: dto 반환

--- src/main/java/com/callbus/community/web/controller/ArticleController.java
+++ src/main/java/com/callbus/community/web/controller/ArticleController.java
@@ -1,22 +1,23 @@
 package com.callbus.community.web.controller;
 
-import com.callbus.community.biz.domain.Article;
-import com.callbus.community.biz.domain.ThumbsUp;
 import com.callbus.community.biz.domain.properties.Message;
 import com.callbus.community.biz.domain.properties.StatusEnum;
 import com.callbus.community.biz.service.ArticleService;
 import com.callbus.community.biz.service.ThumbsUpService;
-import com.callbus.community.web.dto.ReqArticle;
-import java.nio.charset.Charset;
+import com.callbus.community.web.dto.ArticleDto;
+import com.callbus.community.web.dto.ArticleForm;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,36 +32,75 @@ public class ArticleController {
     private final ThumbsUpService thumbsUpService;
 
     @PostMapping("/new")
-    public ResponseEntity<Message> createArticle(@RequestBody ReqArticle articleForm,
-        @RequestHeader(required = false, value = "Authentication") String id) {
-
-        Article article = articleService.createArticle(articleForm, id);
-        Message message = new Message();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
-        message.setStatus(StatusEnum.OK);
+    public ResponseEntity<Message> createArticle(@RequestBody ArticleForm articleForm,
+        @RequestHeader(required = false, value = "Authentication") String accountId) {
+        ArticleDto articleDto = articleService.createArticle(articleForm, accountId);
+        HttpHeaders headers = getHttpHeaders();
+        Message message = getMessage();
         message.setMessage("게시글 등록에 성공했습니다.");
-        message.setData(article);
-
+        message.setData(articleDto);
         return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Article> readArticle(@PathVariable("id") long id) {
-        Article article = articleService.readArticle(id);
-        return new ResponseEntity<>(article, HttpStatus.OK);
+    public ResponseEntity<ArticleDto> readArticle(@PathVariable("id") long id,
+    @RequestHeader(required = false, value = "Authentication") String accountId) {
+        ArticleDto articleDto = articleService.readArticle(id, accountId);
+        return new ResponseEntity<>(articleDto, HttpStatus.OK);
+    }
+    
+    @GetMapping("/list")
+    public ResponseEntity<List<ArticleDto>> readArticles(
+        @RequestHeader(required = false, value = "Authentication") String accountId){
+        List<ArticleDto> articleDtoList = articleService.readArticles(accountId);
+        return new ResponseEntity<>(articleDtoList, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<Message> updateArticle(@PathVariable("id") long articleId,
+        @RequestBody ArticleForm articleForm,
+        @RequestHeader(required = false, value = "Authentication") String accountId){
+        ArticleDto articleDto = articleService.updateArticle(articleId, articleForm, accountId);
+        Message message = getMessage();
+        HttpHeaders headers = getHttpHeaders();
+        message.setMessage("게시글을 수정 했습니다.");
+        message.setData(articleDto);
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<Message> deleteArticle(@PathVariable("id") long articleId,
+        @RequestHeader(required = false, value = "Authentication") String accountId){
+        Message message = getMessage();
+        HttpHeaders headers = getHttpHeaders();
+        message.setMessage(articleService.deleteArticle(articleId, accountId));
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 
     @PostMapping("/{id}/thumbsUp")
-    public ResponseEntity<Message> thumbsUp(@PathVariable("id") long id,
+    public ResponseEntity<Message> createThumbsUp(@PathVariable("id") long id,
         @RequestHeader(required = false, value = "Authentication") String accountId) {
-        Message message = new Message();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
-        message.setStatus(StatusEnum.OK);
-        message.setMessage(thumbsUpService.thumbsUp(id, accountId));
+        Message message = getMessage();
+        HttpHeaders headers = getHttpHeaders();
+        message.setMessage(thumbsUpService.createThumbsUp(id, accountId));
         return new ResponseEntity<>(message, headers, HttpStatus.OK);
-
     }
 
+    @GetMapping("/thumbsUps")
+    public ResponseEntity<List<ArticleDto>> readThumbsUps(@RequestHeader(required = false, value = "Authentication") String accountId){
+        List<ArticleDto> articleDtoList = thumbsUpService.readThumbsUpList(accountId);
+        return new ResponseEntity<>(articleDtoList, HttpStatus.OK);
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+        return headers;
+    }
+
+    private Message getMessage() {
+        Message message = new Message();
+        message.setStatus(StatusEnum.OK);
+        return message;
+    }
 }
