a173572c29271acb7a302c467e079ebb22b909b3
wijoonwu <devjoon@nate.com>
2023-02-14 18:00:32 +0900

    feat. 게시글 좋아요 기능구현

--- src/main/java/com/callbus/community/web/controller/ArticleController.java
+++ src/main/java/com/callbus/community/web/controller/ArticleController.java
@@ -1,14 +1,66 @@
 package com.callbus.community.web.controller;
 
-import org.springframework.stereotype.Controller;
+import com.callbus.community.biz.domain.Article;
+import com.callbus.community.biz.domain.ThumbsUp;
+import com.callbus.community.biz.domain.properties.Message;
+import com.callbus.community.biz.domain.properties.StatusEnum;
+import com.callbus.community.biz.service.ArticleService;
+import com.callbus.community.biz.service.ThumbsUpService;
+import com.callbus.community.web.dto.ReqArticle;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/article")
+@RequiredArgsConstructor
 public class ArticleController {
 
-    @GetMapping("/")
-    public String Welcome(){
-        return "";
+    private final ArticleService articleService;
+    private final ThumbsUpService thumbsUpService;
+
+    @PostMapping("/new")
+    public ResponseEntity<Message> createArticle(@RequestBody ReqArticle articleForm,
+        @RequestHeader(required = false, value = "Authentication") String id) {
+
+        Article article = articleService.createArticle(articleForm, id);
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+        message.setStatus(StatusEnum.OK);
+        message.setMessage("게시글 등록에 성공했습니다.");
+        message.setData(article);
+
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> readArticle(@PathVariable("id") long id) {
+        Article article = articleService.readArticle(id);
+        return new ResponseEntity<>(article, HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/thumbsUp")
+    public ResponseEntity<Message> thumbsUp(@PathVariable("id") long id,
+        @RequestHeader(required = false, value = "Authentication") String accountId) {
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+        message.setStatus(StatusEnum.OK);
+        message.setMessage(thumbsUpService.thumbsUp(id, accountId));
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
+
     }
 
 }
