faed59b2fe44a64ce3e117438669206356fe250f
wijoonwu <devjoon@nate.com>
2023-02-15 01:13:33 +0900

    Update ThumbsUpRepository.java

--- src/main/java/com/callbus/community/biz/repository/ThumbsUpRepository.java
+++ src/main/java/com/callbus/community/biz/repository/ThumbsUpRepository.java
@@ -3,9 +3,11 @@ package com.callbus.community.biz.repository;
 import com.callbus.community.biz.domain.Article;
 import com.callbus.community.biz.domain.Member;
 import com.callbus.community.biz.domain.ThumbsUp;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ThumbsUpRepository extends JpaRepository<ThumbsUp, Long> {
 
     ThumbsUp findByArticleAndMember(Article article, Member member);
+    List<ThumbsUp> findAllByMember(Member member);
 }
