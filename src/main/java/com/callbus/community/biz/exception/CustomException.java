0eafbc5b08b6c2686c7cd280384d8e5a44f0b7f0
wijoonwu <devjoon@nate.com>
2023-02-15 01:12:05 +0900

    Chore: unused import 삭제

--- src/main/java/com/callbus/community/biz/exception/CustomException.java
+++ src/main/java/com/callbus/community/biz/exception/CustomException.java
@@ -1,7 +1,6 @@
 package com.callbus.community.biz.exception;
 
 import com.callbus.community.biz.domain.properties.ErrorCode;
-import java.util.function.Supplier;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
