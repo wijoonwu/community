9220aeb7dd27dfbb734f572d87cc476a9cb4865f
wijoonwu <devjoon@nate.com>
2023-02-15 01:08:47 +0900

    Create ArticleForm.java

---
+++ src/main/java/com/callbus/community/web/dto/ArticleForm.java
@@ -0,0 +1,25 @@
+package com.callbus.community.web.dto;
+
+import com.callbus.community.biz.domain.Article;
+import com.callbus.community.biz.domain.Member;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ArticleForm {
+    private long id;
+    private Member member;
+    private String title;
+    private String content;
+
+    public Article toEntity() {
+        return new Article(member, title, content);
+    }
+
+    public void setMember(Member member){
+        this.member = member;
+    }
+}
