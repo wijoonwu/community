e5de751d18cf70459643b4afc099e19eb33c1e28
wijoonwu <devjoon@nate.com>
2023-02-15 01:12:40 +0900

    Chore: 에러코드 추가

--- src/main/java/com/callbus/community/biz/domain/properties/ErrorCode.java
+++ src/main/java/com/callbus/community/biz/domain/properties/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
 
     MEMBER_NOT_FOUND(401,"외부 사용자는 글을 작성할 수 없습니다."),
     INTERNAL_SERVER_ERROR(500, "서버 에러가 발생했습니다. 고객센터로 문의 바랍니다."),
-    ARTICLE_NOT_FOUND(404, "게시글을 찾을 수 없습니다." );
+    ARTICLE_NOT_FOUND(404, "게시글을 찾을 수 없습니다." ),
+    THE_WRONG_APPROACH(401, "잘못된 접근입니다." );
 
     private final int status;
     private final String msg;
