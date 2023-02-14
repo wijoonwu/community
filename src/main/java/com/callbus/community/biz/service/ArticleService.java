5031f77195d7ce452f31b2c068f5658da8be8c06
wijoonwu <devjoon@nate.com>
2023-02-15 01:11:14 +0900

    Feat: 게시글 수정|삭제 기능 구현

--- src/main/java/com/callbus/community/biz/service/ArticleService.java
+++ src/main/java/com/callbus/community/biz/service/ArticleService.java
@@ -6,9 +6,13 @@ import com.callbus.community.biz.domain.properties.ErrorCode;
 import com.callbus.community.biz.exception.CustomException;
 import com.callbus.community.biz.repository.ArticleRepository;
 import com.callbus.community.biz.repository.MemberRepository;
-import com.callbus.community.web.dto.ReqArticle;
+import com.callbus.community.web.dto.ArticleDto;
+import com.callbus.community.web.dto.ArticleForm;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,23 +25,62 @@ public class ArticleService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Article createArticle(ReqArticle articleForm, String id) {
+    public ArticleDto createArticle(ArticleForm articleForm, String accountId) {
         try {
-            Member member = memberRepository.findByAccountId(id);
-//            articleForm.setMember(member);
-            String writer = member.getNickname() + "(" + member.getAccountType().getDescription() + ")";
-            articleForm.setWriter(writer);
+            Member member = memberRepository.findByAccountId(accountId);
+            articleForm.setMember(member);
             Article article = articleForm.toEntity();
             articleRepository.save(article);
-            return article;
-        } catch (NullPointerException e) {
+            return new ArticleDto(article, accountId);
+        } catch (DataIntegrityViolationException e) {
             throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
         }
     }
+
     @Transactional(readOnly = true)
-    public Article readArticle(long id) {
+    public ArticleDto readArticle(long id, String accountId) {
         Article article = articleRepository.findById(id).orElseThrow(
-            () -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
-        return article;
+        () -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
+        return new ArticleDto(article, accountId);
     }
+
+    @Transactional(readOnly = true)
+    public List<ArticleDto> readArticles(String accountId) {
+        List<Article> articleList = articleRepository.findAll();
+        List<ArticleDto> articleDtoList = new ArrayList<>();
+        for(Article article : articleList){
+            articleDtoList.add(new ArticleDto(article, accountId));
+        }
+        return articleDtoList;
+    }
+
+    @Transactional
+    public ArticleDto updateArticle(long articleId, ArticleForm articleForm, String accountId) {
+        Article article = articleRepository.findById(articleId).orElseThrow(
+            () -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
+        Member member = memberRepository.findByAccountId(accountId);
+
+        if(article.getMember() != member){
+           throw new CustomException(ErrorCode.THE_WRONG_APPROACH);
+        }
+
+        article.setTitle(articleForm.getTitle());
+        article.setContent(articleForm.getContent());
+        return new ArticleDto(article, accountId);
+    }
+
+    @Transactional
+    public String deleteArticle(long articleId, String accountId) {
+        Article article = articleRepository.findById(articleId).orElseThrow(
+            () -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
+        Member member = memberRepository.findByAccountId(accountId);
+
+        if(article.getMember() != member){
+            throw new CustomException(ErrorCode.THE_WRONG_APPROACH);
+        }
+
+        articleRepository.delete(article);
+        return "게시글을 삭제 했습니다";
+    }
+
 }
