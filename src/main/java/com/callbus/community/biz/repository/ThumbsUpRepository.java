4c8ac1e54447f256f3c8f909f6ab44fb159a5407
wijoonwu <devjoon@nate.com>
2023-02-14 17:16:52 +0900

    Create ThumbsUpRepository.java

---
+++ src/main/java/com/callbus/community/biz/repository/ThumbsUpRepository.java
@@ -0,0 +1,8 @@
+package com.callbus.community.biz.repository;
+
+import com.callbus.community.biz.domain.ThumbsUp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThumbsUpRepository extends JpaRepository<ThumbsUp, Long> {
+
+}
