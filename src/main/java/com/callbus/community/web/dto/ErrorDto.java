ef512b9b1fb311ba35582e7f81f6ab97621c412a
wijoonwu <devjoon@nate.com>
2023-02-14 17:17:49 +0900

    Create ErrorDto.java

---
+++ src/main/java/com/callbus/community/web/dto/ErrorDto.java
@@ -0,0 +1,12 @@
+package com.callbus.community.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorDto {
+
+    private final int status;
+    private final String msg;
+}
