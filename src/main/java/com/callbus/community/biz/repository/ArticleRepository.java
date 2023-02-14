e8efef0852f9078a573cd43d673868676f56ad16
wijoonwu <devjoon@nate.com>
2023-02-15 01:13:31 +0900

    Update ArticleRepository.java

--- src/main/java/com/callbus/community/biz/repository/ArticleRepository.java
+++ src/main/java/com/callbus/community/biz/repository/ArticleRepository.java
@@ -1,8 +1,10 @@
 package com.callbus.community.biz.repository;
 
 import com.callbus.community.biz.domain.Article;
+import com.callbus.community.biz.domain.ThumbsUp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 
+    Article findByThumbsUp(ThumbsUp thumbsUp);
 }
