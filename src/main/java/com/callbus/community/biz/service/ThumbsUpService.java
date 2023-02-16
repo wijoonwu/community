d2b1b02470e54763be93706609044e309ff0cdbc
wijoonwu <devjoon@nate.com>
2023-02-16 19:37:55 +0900

    Refactor: isEmpty -> orElseThrow 변경

--- src/main/java/com/callbus/community/biz/service/ThumbsUpService.java
+++ src/main/java/com/callbus/community/biz/service/ThumbsUpService.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -29,42 +28,33 @@ public class ThumbsUpService {
     public String createThumbsUp(long id, String accountId) {
         Article article = articleRepository.findById(id).orElseThrow(
             () -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
-        Optional<Member> member = memberRepository.findByAccountId(accountId);
-        if (ObjectUtils.isEmpty(member)) {
-            throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+        Member member = memberRepository.findByAccountId(accountId).orElseThrow(
+            () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        Optional<ThumbsUp> target = thumbsUpRepository.findByArticleAndMember(article, member);
+        if(target.isPresent()){
+            target.get().ThumbsDown();
+            thumbsUpRepository.delete(target.get());
+            return "좋아요가 취소되었습니다.";
         }
-        if(member.isPresent()){
-            Optional<ThumbsUp> target = thumbsUpRepository.findByArticleAndMember(article, member.get());
-            if(target.isPresent()){
-                target.get().ThumbsDown();
-                thumbsUpRepository.delete(target.get());
-                return "좋아요가 취소되었습니다.";
-            }
-            ThumbsUp thumbsUp = new ThumbsUp(member.get(), article);
-            thumbsUpRepository.save(thumbsUp);
-            article.setThumbsUp(thumbsUp);
-        }
+        ThumbsUp thumbsUp = new ThumbsUp(member, article);
+        thumbsUpRepository.save(thumbsUp);
+        article.setThumbsUp(thumbsUp);
         return "좋아요를 눌렀습니다.";
     }
 
     @Transactional(readOnly = true)
     public List<ResArticle> readThumbsUpList(String accountId) {
-        Optional<Member> member = memberRepository.findByAccountId(accountId);
         List<ResArticle> articleDtoList = new ArrayList<>();
-        if(member.isPresent()) {
-            List<ThumbsUp> thumbsUps = thumbsUpRepository.findAllByMember(member.get());
-            try {
-                for (ThumbsUp thumbsUp : thumbsUps) {
-                    Optional<Article> article = articleRepository.findByThumbsUp(thumbsUp);
-                    if (article.isPresent()) {
-                        if (article.get().getDeletedDate() == null) {
-                            ResArticle resArticle = new ResArticle(article.get(), accountId);
-                            articleDtoList.add(resArticle);
-                        }
-                    }
+        Member member = memberRepository.findByAccountId(accountId).orElseThrow(
+            () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        List<ThumbsUp> thumbsUps = thumbsUpRepository.findAllByMember(member);
+        for (ThumbsUp thumbsUp : thumbsUps) {
+            Optional<Article> article = articleRepository.findByThumbsUp(thumbsUp);
+            if(article.isPresent()){
+                if (article.get().getDeletedDate() == null) {
+                    ResArticle resArticle = new ResArticle(article.get(), accountId);
+                    articleDtoList.add(resArticle);
                 }
-            } catch (NullPointerException e) {
-                e.printStackTrace();
             }
         }
         return articleDtoList;
